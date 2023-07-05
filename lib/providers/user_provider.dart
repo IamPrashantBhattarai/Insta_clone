46a03c72b2d1a0c8f8f8b3feff7d31209d947733
Prashant <prashantbhattarainepal@gmail.com>
2023-07-05 10:51:02 +0545

    Adding providers into the file

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:instagram_clone/models/user.dart';
+import 'package:instagram_clone/resources/auth_methods.dart';
 
 class UserProvider with ChangeNotifier {
   //Creating the global variable
   //Make sure to make this user a private field
   User? _user;
+  final AuthMethods _authMethods = AuthMethods();
   User get getUser => _user!;
 
-  Future<void> refreshUser() async {}
+  Future<void> refreshUser() async {
+    User user = await _authMethods.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
 }
