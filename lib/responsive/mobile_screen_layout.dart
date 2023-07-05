73424bd7f653899fc6717538dcafdb535f03c1b5
Prashant <prashantbhattarainepal@gmail.com>
2023-07-05 11:01:35 +0545

    state management almost complete

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:instagram_clone/models/user.dart' as model;
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -10,30 +13,29 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
-  String username = "";
+  //Another one time way of state management
+  // String username = "";
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getUsername();
+  // }
+  // void getUsername() async {
+  //   DocumentSnapshot snap = await FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc(FirebaseAuth.instance.currentUser!.uid)
+  //       .get();
+  //   setState(() {
+  //     username = (snap.data() as Map<String, dynamic>)['username'];
+  //   });
+  // }
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getUsername();
-  }
-
-  void getUsername() async {
-    DocumentSnapshot snap = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .get();
-    setState(() {
-      username = (snap.data() as Map<String, dynamic>)['username'];
-    });
-  }
-
-  @override
+  model.User user = Provider.of<UserProvider>(context).getUser;
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("$username"),
+        child: Text("Mobile Screen"),
       ),
     );
   }
