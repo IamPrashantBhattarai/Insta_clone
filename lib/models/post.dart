4640181f2090dc2f7b513e81d77e55f1f40eba8f
Prashant <prashantbhattarainepal@gmail.com>
2023-07-18 23:58:26 +0545

    Post model started

---
+++ lib/models/post.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String description;
+  final String uid;
+  final String username;
+  final String postId;
+  final String datePublished;
+  final String postUrl;
+  final String profImage;
+  final likes;
+
+  const User({
+    required this.description,
+    required this.uid,
+    required this.username,
+    required this.postId,
+    required this.datePublished,
+    required this.postUrl,
+    required this.profImage,
+    required this.likes,
+  });
+
+//This function toJson will create the object file of class User
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "uid": uid,
+        "email": email,
+        "photoUrl": photoUrl,
+        "bio": bio,
+        "followers": followers,
+        "following": following,
+      };
+
+  static User fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+    return User(
+      username: snapshot['username'],
+      uid: snapshot['uid'],
+      email: snapshot['email'],
+      photoUrl: snapshot['photoUrl'],
+      bio: snapshot['bio'],
+      followers: snapshot['followers'],
+      following: snapshot['following'],
+    );
+  }
+}
