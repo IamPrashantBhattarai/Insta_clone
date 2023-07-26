81a0b15c4f952bcab916dadca28a5e64ac670738
Prashant <prashantbhattarainepal@gmail.com>
2023-07-26 23:32:44 +0545

    UUID dependency added

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -15,6 +15,11 @@ class StorageMethods {
     Reference ref =
         _storage.ref().child(childName).child(_auth.currentUser!.uid);
 
+    if (isPost) {
+      String id = const Uuid().v1();
+      ref.child(id);
+    }
+
     UploadTask uploadTask = ref.putData(file);
 
     TaskSnapshot snap = await uploadTask;
