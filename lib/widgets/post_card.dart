6cb2724adeaf1075b4465f972eeafad0ea60b4d4
Prashant <prashantbhattarainepal@gmail.com>
2023-08-03 00:35:34 +0545

    Card items being added

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -8,6 +8,47 @@ class PostCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: mobileBackgroundColor,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 17,
+            ).copyWith(right: 0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 16,
+                  backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1687360440613-7e002b7261da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=700&q=60",
+                  ),
+                ),
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 0,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "username",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ))
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
