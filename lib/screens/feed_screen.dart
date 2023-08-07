e766037c040627ac4d62d404e70e15182d3d8249
Prashant <prashantbhattarainepal@gmail.com>
2023-08-07 22:22:45 +0545

    Stream builder widget added

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/models/post.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/widgets/post_card.dart';
 
@@ -26,7 +28,21 @@ class FeedScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const PostCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => PostCard(),
+          );
+        },
+      ),
     );
   }
 }
